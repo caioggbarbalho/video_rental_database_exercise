dead3de66ba1b220caf30c62463f15ebac090798
Caio Barbalho <caiobarbalho@outlook.com>
2023-11-18 17:17:03 -0300

    Deleted some example files

--- exemplo prof/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$host = "localhost"; 			
-	$user = "root"; 
-	$pass = ""; 
-	$banco = "banco";
-		
-	$conexao = @mysqli_connect($host, $user, $pass, $banco ) 
-	or die ("Problemas com a conex�o do Banco de Dados");
-?>
